a7f6a280477eb8654620c4ebb1d262bba7154785
Chitranshu Sanket <sinhachitranshu40@gmail.com>
2026-06-22 21:05:10 +0530

    add one more data

--- DVC/add_data.py
+++ DVC/add_data.py
@@ -3,9 +3,9 @@ import pandas as pd
 df = pd.read_csv(r'data\sample_data.csv')
 
 new_df = pd.DataFrame({
-    'name': ['Raina'],
-    'Age': [21],
-    'city': ['Kerala']
+    'name': ['Divya'],
+    'Age': [22],
+    'city': ['Deradun']
 })
 
 df = pd.concat([df, new_df], ignore_index=True)
